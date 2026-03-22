d4c0fbe20849b14d10ce535375f14ce76a18d635
subhradeepnag <subhradeepnag31@gmail.com>
2026-03-22 20:21:24 +0530

    update create activity route

--- client/src/app/createActivity/page.tsx
+++
@@ -1,8 +0,0 @@
-import ActivityForm from '@/components/ActivityForm'
-import React from 'react'
-
-const createActivity = () => {
-  return <ActivityForm action="create" />
-}
-
-export default createActivity
